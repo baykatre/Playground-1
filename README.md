5bfe3f3a24f95ce16ff057b369691c1da25f4dc4
info@anilozturk.co <Qaqta3-mewmec-seqpuc>
2021-09-02 21:57:40 +0300

    Readme Update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Playground-1
+
+Playground 1 is my personal playing area by Java and Spring Boot. My main aim is improving my coding skills. Also remembering and updating my technical know-how. 
+
+## Aim
+Specially, modules in the app will be simple and easy logic but I want to use algorithms that not much used. I hope I will be success.
--- src/main/java/co/anilozturk/playground/calculator/operation/Operation.java
+++ src/main/java/co/anilozturk/playground/calculator/operation/Operation.java
@@ -4,13 +4,15 @@ import co.anilozturk.playground.exception.CalculatorException;
 
 public abstract class Operation {
 
+    public static final String ARITHMETIC_ERROR = "Arithmetic Error!";
+
     protected abstract OperationOutput process(OperationInput input);
 
     public OperationOutput wrapProcess(OperationInput input) {
         try {
             return process(input);
         } catch (ArithmeticException e) {
-            throw new CalculatorException("Arithmetic Error!");
+            throw new CalculatorException(ARITHMETIC_ERROR);
         }
     }
 }
