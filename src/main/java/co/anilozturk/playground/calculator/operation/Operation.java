15adaf4e30acc4ceb3081a0033bd6343d073c2d3
info@anilozturk.co <info@anilozturk.co>
2021-09-03 19:33:37 +0300

    refactor

--- src/main/java/co/anilozturk/playground/calculator/operation/Operation.java
+++ src/main/java/co/anilozturk/playground/calculator/operation/Operation.java
@@ -4,15 +4,13 @@ import co.anilozturk.playground.exception.CalculatorException;
 
 public abstract class Operation {
 
-    public static final String ARITHMETIC_ERROR = "Arithmetic Error!";
-
     protected abstract OperationOutput process(OperationInput input);
 
     public OperationOutput wrapProcess(OperationInput input) {
         try {
             return process(input);
         } catch (ArithmeticException e) {
-            throw new CalculatorException(ARITHMETIC_ERROR);
+            throw new CalculatorException("Arithmetic Error!");
         }
     }
 }
